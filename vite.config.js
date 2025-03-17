caf4ba587f568508c307bf7a97677cf619d0af57
yurishe0 <yurishe00@gmail.com>
2025-03-17 10:44:19 +0100

    fix: subsite deployment issue

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         paznokcie: resolve(__dirname, 'szkolenia/stylizacja-paznokci.html'),
+        wybielanie: resolve(__dirname, 'szkolenia/wybielanie-zębów.html')
       }
     }
   }
